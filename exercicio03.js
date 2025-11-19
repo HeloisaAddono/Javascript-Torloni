e90d52695a9940c6444939efbd9e98a814106467
HELOÍSA PEREIRA ADDONO <54202070879@imaccommand002.local>
2025-11-19 14:12:16 -0300

    exercicio03

---
+++ exercicio03.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite o número que desejar :"));
+
+if (numero % 2 === 0) {
+    alert("O número " + numero + " é PAR.");
+} else {
+    alert("O número " + numero + " é ÍMPAR.");
+}
